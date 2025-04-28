ae4a44186d82d1e80a19ed34160a73f13a829183
Ankit <ankit@gmail.com>
2025-04-28 17:11:03 +0530

    files added

---
+++ routes/user.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+const { saveRedirectUrl } = require("../middleware.js");
+
+const userController = require("../controllers/users.js");
+
+router.route("/signup")
+.get(userController.renderSignUpForm)
+.post(wrapAsync(userController.signUp));
+
+router.route("/login")
+.get(userController.renderloginForm)
+.post(saveRedirectUrl, passport.authenticate("local", {
+        failureRedirect: "/login",
+        failureFlash: true,
+    }),
+    userController.login
+);
+ 
+router.get("/logout", userController.logout);
+
+module.exports = router; 
